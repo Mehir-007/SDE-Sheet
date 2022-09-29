5fba2bcb754d3771d3b2d9f56a2e3cbc8cadd58e
Mehir_007 <mehirrouth20000@gmail.com>
2022-09-29 23:41:58 +0530

    Linked List Day 5

---
+++ DAY 5(LINKED LIST)/Middle_Of_A_LL.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+ListNode* middleNode(ListNode* head) {
+        ListNode *slow=head,*fast=head;
+        while(fast)
+        {
+            fast=fast->next;
+            if(fast)
+            {
+                fast=fast->next;
+                slow=slow->next;
+            }
+        }
+        
+        return slow;
+    }
+
+int main()
+{    
+}
