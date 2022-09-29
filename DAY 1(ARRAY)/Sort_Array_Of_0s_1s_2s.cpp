dee043968b98c6aae44cb7b1d8a26d39a848d30d
Mehir_007 <mehirrouth20000@gmail.com>
2022-09-29 18:27:55 +0530

    Array Day 1

---
+++ DAY 1(ARRAY)/Sort_Array_Of_0s_1s_2s.cpp
@@ -0,0 +1,36 @@
+/*
+Problem Statement: Given an array consisting of only 0s, 1s and 2s. 
+Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+      int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+                
+            }
+        }
+}
+int main()
+{
+    
+}
