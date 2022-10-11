88b8e2f296b192d1a555da9b5a6ae0be0e3ae1fc
Mehir_007 <mehirrouth20000@gmail.com>
2022-10-11 12:05:35 +0530

    Array Day 2

---
+++ DAY 2(ARRAY PART II)/Merge_Overlapping_Intervals.cpp
@@ -0,0 +1,44 @@
+/*Problem Statement: Given an array of intervals, merge all the overlapping intervals and return an array of non-overlapping intervals.
+Example 1: 
+
+Input: intervals=[[1,3],[2,6],[8,10],[15,18]]
+
+Output: [[1,6],[8,10],[15,18]]
+
+Explanation: Since intervals [1,3] and [2,6] are overlapping we can merge them to form [1,6]
+ intervals.*/
+#include<bits/stdc++.h>
+
+using namespace std;
+vector < vector < int >> merge(vector < vector < int >> & intervals) {
+
+  sort(intervals.begin(), intervals.end());
+  vector < vector < int >> merged;
+
+  for (int i = 0; i < intervals.size(); i++) {
+    if (merged.empty() || merged.back()[1] < intervals[i][0]) {
+      vector < int > v = {
+        intervals[i][0],
+        intervals[i][1]
+      };
+
+      merged.push_back(v);
+    } else {
+      merged.back()[1] = max(merged.back()[1], intervals[i][1]);
+    }
+  }
+
+  return merged;
+}
+
+int main() {
+  vector < vector < int >> arr;
+  arr = {{1, 3}, {2, 4}, {2, 6}, {8, 9}, {8, 10}, {9, 11}, {15, 18}, {16, 17}};
+  vector < vector < int >> ans = merge(arr);
+
+  cout << "Merged Overlapping Intervals are " << endl;
+
+  for (auto it: ans) {
+    cout << it[0] << " " << it[1] << "\n";
+  }
+}
