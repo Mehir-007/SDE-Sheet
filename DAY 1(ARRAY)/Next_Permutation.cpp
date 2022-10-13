117bdadcbc36ff886aa9dac2b1db87715a63e95d
Mehir_007 <mehirrouth20000@gmail.com>
2022-10-13 12:34:28 +0530

    Array Day 1

---
+++ DAY 1(ARRAY)/Next_Permutation.cpp
@@ -0,0 +1,16 @@
+/*
+Given an array Arr[] of integers, rearrange the numbers of the given array into the lexicographically next greater permutation of numbers.
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+Input format: Arr[] = {1,3,2}
+Output: Arr[] = {2,1,3}
+
+Input format: Arr[] = {3,2,1}
+Output: Arr[] = {1,2,3}
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+}
