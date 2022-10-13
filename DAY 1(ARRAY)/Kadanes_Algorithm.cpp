9b18af97630e48f3171ca200a8d04208e20b643f
Mehir_007 <mehirrouth20000@gmail.com>
2022-10-13 13:26:47 +0530

    Array Day 1

--- DAY 1(ARRAY)/Kadanes_Algorithm.cpp
+++ DAY 1(ARRAY)/Kadanes_Algorithm.cpp
@@ -13,9 +13,27 @@ Output: 1
 Explanation: Array has only one element and which is giving positive sum of 1.
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int sum = 0;
+    int maxi = INT_MIN;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum = sum + nums[i];
+        maxi = max(sum, maxi);
+        if (sum < 0)
+            sum = 0;
+    }
+    return maxi;
+}
+
 int main()
 {
+    vector<int> arr{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int lon = maxSubArray(arr);
+    cout << "The longest subarray with maximum sum is " << lon << endl;
     
 }
