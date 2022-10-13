4808de7b56034951b4c01084228baaf34065008b
Mehir_007 <mehirrouth20000@gmail.com>
2022-10-13 13:32:10 +0530

    Linked List Day 5

---
+++ DAY 5(LINKED LIST)/Delete_Node.cpp
@@ -0,0 +1,18 @@
+/*
+Write a function to delete a node in a singly-linked list. 
+You will not be given access to the head of the list instead, you will be given access to the node to be deleted directly. 
+It is guaranteed that the node to be deleted is not a tail node in the list.
+Input:
+ Linked list: [1,4,2,3]
+       Node = 2
+Output:
+ Linked list: [1,4,3]
+Explanation: Access is given to node 2. After deleting nodes, the linked list will be modified to [1,4,3].
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+}
