1c9051fdb086250ed81df15ab9c083cdea9c0c25
Mehir_007 <mehirrouth20000@gmail.com>
2022-10-13 13:06:40 +0530

    Array Day 1

--- DAY 1(ARRAY)/Kadanes_Algorithm.cpp
+++ DAY 1(ARRAY)/Kadanes_Algorithm.cpp
@@ -3,23 +3,19 @@ Problem Statement: Given an integer array arr, find the contiguous subarray (con
 has the largest sum and return its sum and print the subarray
 
 Example 1:
-Input: arr = [-2,1,-3,4,-1,2,1,-5,4] 
-Output: 6 
-Explanation: [4,-1,2,1] has the largest sum = 6. 
+Input: arr = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
 
-Examples 2: 
-Input: arr = [1] 
-Output: 1 
-Explanation: Array has only one element and which is giving positive sum of 1. 
+Examples 2:
+Input: arr = [1]
+Output: 1
+Explanation: Array has only one element and which is giving positive sum of 1.
 */
-
 
 #include<bits/stdc++.h>
 using namespace std;
-
-
-
 int main()
 {
-   
+    
 }
--- DAY 1(ARRAY)/Next_Permutation.cpp
+++ DAY 1(ARRAY)/Next_Permutation.cpp
@@ -7,10 +7,32 @@ Output: Arr[] = {2,1,3}
 Input format: Arr[] = {3,2,1}
 Output: Arr[] = {1,2,3}
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+void nextPermutation(vector<int> &nums)
+{
+    if (nums.size() == 0 || nums.size() == 1)
+        return;
+    int i = nums.size() - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1])
+        i--;
+    if (i >= 0)
+    {
+        int j = nums.size() - 1;
+        while (nums[j] <= nums[i])
+            j--;
+        swap(nums[i], nums[j]);
+    }
+    reverse(nums.begin() + i + 1, nums.end());
+}
 
 int main()
 {
-    
+    vector<int> v = {2, 1, 3};
+    nextPermutation(v);
+    for(auto x:v)
+    {
+        cout<<x<<" ";
+    }
 }
