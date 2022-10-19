91a84448253b8b01c7a385fa3b65e0bf7b6353d6
Mehir_007 <mehirrouth20000@gmail.com>
2022-10-19 09:08:58 +0530

    Array Day 2

---
+++ DAY 2(ARRAY PART II)/Rotate_Matrix.cpp
@@ -0,0 +1,17 @@
+/*
+Problem Statement: Given a matrix, your task is to rotate the matrix 90 degrees clockwise.
+Example 1:
+
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Explanation: Rotate the matrix simply by 90 degree clockwise and return the matrix.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+}
