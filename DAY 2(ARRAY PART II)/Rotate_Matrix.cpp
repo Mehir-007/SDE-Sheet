60db3e2bb94f85fac5e469acd20c18457299c2d2
Mehir_007 <mehirrouth20000@gmail.com>
2022-10-19 09:25:44 +0530

    Array Day 2

--- DAY 2(ARRAY PART II)/Rotate_Matrix.cpp
+++ DAY 2(ARRAY PART II)/Rotate_Matrix.cpp
@@ -8,10 +8,45 @@ Output: [[7,4,1],[8,5,2],[9,6,3]]
 
 Explanation: Rotate the matrix simply by 90 degree clockwise and return the matrix.
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
+void rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < m; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
 int main()
 {
-    
+    vector<vector<int>> arr;
+    arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    cout << "Original Image" << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    rotate(arr);
+    cout << "Rotated Image" << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
