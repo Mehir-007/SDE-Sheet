7b70558a4d94998779b27178a7461e32bd39fad1
Mehir_007 <mehirrouth20000@gmail.com>
2022-12-24 07:53:17 +0530

    Array Day 2

---
+++ DAY 2(ARRAY PART II)/Merge_Two_Sorted_Array.cpp
@@ -0,0 +1,10 @@
+/*
+Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. 
+Merge them in sorted order. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+}
